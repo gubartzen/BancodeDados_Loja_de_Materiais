5e9e0aab7d3f7c494ede80233001652370b1124e
gubartzen <108465498+gubartzen@users.noreply.github.com>
2023-09-27 09:13:23 -0300

    Add files via upload

---
+++ dump.sqlite.sql
@@ -0,0 +1,15 @@
+-- TABLE
+CREATE TABLE Funcionarios(
+"idFuncionarios" INT
+, 'funcnome' varchar(45), 'funcendereco' varchar(45), 'funcidade' varchar(45), 'idItens' varchar(45), 'idGerente' varchar(45), 'idCaixa' varchar(45), 'idSupridores' varchar(45), 'idSeguranca' varchar(45));
+CREATE TABLE LojaDeMateriais(
+"CNPJ" Int,
+"Endereço" varchar(45)
+, "lojaconta" INT, "idItens" int);
+ 
+-- INDEX
+ 
+-- TRIGGER
+ 
+-- VIEW
+ 
