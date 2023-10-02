cb8126e714e40a1b212972edceaca3335f00cd58
Christian Augustus <108826449+Jujubileuu@users.noreply.github.com>
2023-10-02 08:51:23 -0300

    Insert dos dados
    Script de inserção dos dados no modelo SQL

--- dump.sqlite.sql
+++ dump.sqlite.sql
@@ -1,11 +1,5 @@
 -- TABLE
-CREATE TABLE Funcionarios(
-"idFuncionarios" INT
-, 'funcnome' varchar(45), 'funcendereco' varchar(45), 'funcidade' varchar(45), 'idItens' varchar(45), 'idGerente' varchar(45), 'idCaixa' varchar(45), 'idSupridores' varchar(45), 'idSeguranca' varchar(45));
-CREATE TABLE LojaDeMateriais(
-"CNPJ" Int,
-"Endereço" varchar(45)
-, "lojaconta" INT, "idItens" int);
+CREATE TABLE demo (ID integer primary key, Name varchar(20), Hint text );
  
 -- INDEX
  
